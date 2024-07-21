6f4fdf8cc37ca0edb26e089f8ff868753dcbcc7b
wuwudy <2806067876@qq.com>
2024-07-21 19:49:26 +0800

    测试lint-stage 1

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import createRoute from './router'
+const router = createRoute()
+console.log(router)
+</script>
+
+<template>
+  <div>
+    我是App
+    <table></table>
+  </div>
+</template>
+
+<style scoped></style>
